822438b249a4aba026ee53e83d21b602972bd8cf
Ostap Orishko <ozorishko@gmail.com>
2020-06-25 00:33:27 +0100

    Updates

---
+++ precache-manifest.720035f66bbcb88b7968d4a9e1cb681f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cddb6ade27e5c21edfb56e60908ea394",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "36ffa0bcb3acfc2d3c1c",
+    "url": "/react-weather-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "db6b00a213ea5edc163b",
+    "url": "/react-weather-app/static/css/main.d686bae8.chunk.css"
+  },
+  {
+    "revision": "36ffa0bcb3acfc2d3c1c",
+    "url": "/react-weather-app/static/js/2.1f5ffbe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.1f5ffbe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db6b00a213ea5edc163b",
+    "url": "/react-weather-app/static/js/main.5a37cb35.chunk.js"
+  },
+  {
+    "revision": "3af16c8b66021af71e04",
+    "url": "/react-weather-app/static/js/runtime-main.9bd7c6ee.js"
+  },
+  {
+    "revision": "b931d84d75a673fdd8ae1134fefd98ba",
+    "url": "/react-weather-app/static/media/cloudy-day.b931d84d.svg"
+  },
+  {
+    "revision": "ef1c6aa1ca72ea2157736318d24ede62",
+    "url": "/react-weather-app/static/media/cloudy-night.ef1c6aa1.svg"
+  },
+  {
+    "revision": "7f93fbdb8a9d02fe414c0a0119b77bd4",
+    "url": "/react-weather-app/static/media/day.7f93fbdb.svg"
+  },
+  {
+    "revision": "770fb8720996e6ff67430f48db5b2409",
+    "url": "/react-weather-app/static/media/mountains.770fb872.png"
+  },
+  {
+    "revision": "a3f3f38d2c17971938ebd891fb6bbd0c",
+    "url": "/react-weather-app/static/media/night.a3f3f38d.svg"
+  },
+  {
+    "revision": "2bd236411dc612e871d9c314239c3fa7",
+    "url": "/react-weather-app/static/media/rainy.2bd23641.svg"
+  },
+  {
+    "revision": "3def7c9fb95731ac622871fb6925d5f6",
+    "url": "/react-weather-app/static/media/snowy.3def7c9f.svg"
+  }
+]);
